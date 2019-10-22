47162df00697fc4d70eed4a35be20f914c8d26bd
RasmusStromblad <56510265+RasmusStromblad@users.noreply.github.com>
2019-10-22 13:31:35 +0200

    Test

---
+++ src/Ovning/Ovn1.java
@@ -0,0 +1,11 @@
+package Ovning;
+
+public class Ovn1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Rasmus Strömblad");
+		System.out.println("Maskrosvägen 7");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module ovn1 {
+}
